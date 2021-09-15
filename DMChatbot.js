bee963658590e4b1566bb424782af98b85d6485c
fleurdelys <npt201083@gmail.com>
2021-09-15 20:45:27 +0700

    Add files via upload
    ad snippet of DM chabot in a js file

---
+++ DMChatbot.js
@@ -0,0 +1,3 @@
+<!-- DM bot - ManyChat -->
+<script src="//widget.manychat.com/124486194319938.js" defer="defer"></script>
+<script src="https://mccdn.me/assets/js/widget.js" defer="defer"></script>
