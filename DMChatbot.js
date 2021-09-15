851f18bbd80410729da7114e6129fe6ba1f42fc8
fleurdelys <npt201083@gmail.com>
2021-09-16 02:32:09 +0700

    Update DMChatbot.js
    document.writeln

--- DMChatbot.js
+++ DMChatbot.js
@@ -1,5 +1,2 @@
-document.write('
-<!-- DM bot - ManyChat -->
-<script src="//widget.manychat.com/124486194319938.js" defer="defer"></script>
-<script src="https://mccdn.me/assets/js/widget.js" defer="defer"></script>
-');
+document.writeln("<script src="//widget.manychat.com/124486194319938.js" defer="defer"></script>");
+document.writeln("<script src="https://mccdn.me/assets/js/widget.js" defer="defer"></script>");
